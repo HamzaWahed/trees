4f0e2375e4e1203f094671bf8acde28475cd9ffb
wahed <hm682264@dal.ca>
2024-03-28 13:49:38 -0300

    add prelim functions

--- src/trees/van_emde_boas.go
+++ src/trees/van_emde_boas.go
@@ -1,9 +1,28 @@
 package trees
 
+import "math"
+
 type vEB struct {
-	Summary *vEB
-	U       int
-	Cluster []*vEB
-	Min     int
-	Max     int
+	Summary          *vEB
+	U                int
+	lowerSquareRoot  int
+	higherSquareRoot int
+	Cluster          []*vEB
+	Min              int
+	Max              int
+}
+
+func low(x int, u int) int {
+	lfloor := int(math.Pow(2, math.Floor(math.Log(float64(u)/2))))
+	return x % lfloor
+}
+
+func high(x int, u int) int {
+	lfloor := int(math.Pow(2, math.Floor(math.Log(float64(u)/2))))
+	return int(math.Floor(float64(x / lfloor)))
+}
+
+func index(x int, y int, u int) int {
+	lfloor := int(math.Pow(2, math.Floor(math.Log(float64(u)/2))))
+	return x*lfloor + y
 }
