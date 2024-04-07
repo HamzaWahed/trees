19585ba1787bd3ff6105fd710622a9c8698a7db9
Anas <anas.n.alhadi@outlook.com>
2024-04-07 08:58:36 -0300

    size, splay time, veb time

--- src/generate_input.py
+++ src/generate_input.py
@@ -6,15 +6,14 @@ REQUEST_RANGE = pow(2, 32)
 
 # Takes in a string to identify the file, ideally a countable value
 def uniform_distribution(number_of_requests):
-    for file_number in range(1, 15):
+    for file_number in range(1, 51):
         elements = rng.uniform(low=0, high=REQUEST_RANGE - 1, size=number_of_requests)
-        np.savetxt("inputs/uniform/" + str(number_of_requests)+"."+str(file_number), elements, fmt="%d")
+        np.savetxt("inputs/uniform/" + str(file_number), elements, fmt="%d")
 
 
 # Takes in a string to identify the file, ideally a countable value
 def mtf_optimal(number_of_requests):
-
-    for file_number in range(1, 15):
+    for file_number in range(1, 51):
         subrange = rng.uniform(low=1, high=number_of_requests / 10, size=1).astype(int)[0]
         number_of_distinct_elements = (number_of_requests / subrange).astype(int)
 
@@ -23,27 +22,9 @@ def mtf_optimal(number_of_requests):
         for i in elements:
             for j in range(0, subrange):
                 to_save.append(i)
-        np.savetxt("inputs/mtf_opt/"+str(number_of_requests)+"."+str(file_number), to_save, fmt="%d")
+        np.savetxt("inputs/mtf_opt/" + str(file_number), to_save, fmt="%d")
 
 
 if __name__ == '__main__':
-    mtf_optimal(100)
-    uniform_distribution(100)
-
     mtf_optimal(1000)
     uniform_distribution(1000)
-
-    mtf_optimal(10000)
-    uniform_distribution(10000)
-
-    mtf_optimal(50000)
-    uniform_distribution(50000)
-
-    mtf_optimal(100000)
-    uniform_distribution(100000)
-
-    mtf_optimal(500000)
-    uniform_distribution(500000)
-
-    mtf_optimal(1000000)
-    uniform_distribution(1000000)
--- src/main/main.go
+++ src/main/main.go
@@ -3,34 +3,35 @@ package main
 import (
 	"fmt"
 	"log"
+	"math"
 	"os"
 	"strconv"
 	"time"
 	"trees/trees"
 )
 
-const NUMBER_OF_REQUESTS int = 65536
-const SMALL int = 512
-const MEDIUM int = 65536
-const LARGE int = 4294967296
 const BECNHMARK_UNIFORM = "-u"
 const BECHMARK_MTF = "-m"
 const BENCHMARK_ALL = "-a"
-const NUMBER_OF_INPUT_FILES = 31
+
+const NUMBER_OF_REQUESTS int = 1000
+const NUMBER_OF_INPUT_FILES = 50
+const MAX_TREE_SIZE int = 16384
+
+var NUMBER_OF_OUTPUTS = int(math.Log2(float64(MAX_TREE_SIZE))) * NUMBER_OF_INPUT_FILES
 
 /*
 *
 function runs the input through both data structures and returns the times they took each. (splay, veb)
 */
-func benchmark(size int, method string) (time.Duration, time.Duration) {
-	var current_range int = size
+func benchmark(size int, input_path string) (time.Duration, time.Duration) {
 	var file *os.File
 	var input []int = make([]int, NUMBER_OF_REQUESTS)
 	var cur int
 	var err error
 
 	// reading in elem from the input files and saving them in an array
-	var path = string("../src/" + method)
+	var path = string("../src/inputs/" + input_path)
 	file, err = os.OpenFile(path, os.O_RDONLY, 0644)
 	if err != nil {
 		panic("couldn't read file, uniform/small.in")
@@ -41,20 +42,20 @@ func benchmark(size int, method string) (time.Duration, time.Duration) {
 		if err != nil {
 			break
 		}
-		input[i] = cur
+		input[i] = cur % size
 	}
 
 	//initializing the splay and vEm Trees
-	var vEM *trees.VEB = trees.BuildVEB(current_range)
+	var vEM *trees.VEB = trees.BuildVEB(size)
 	var splay *trees.SplayTree = trees.NewSplayTree(0)
-	for i := 1; i < current_range; i++ {
+	for i := 1; i < size; i++ {
 		splay.Insert(i)
 	}
 
 	var splay_time time.Duration
 	var vEM_time time.Duration
 
-	for i := 0; i < current_range; i++ {
+	for i := 0; i < NUMBER_OF_REQUESTS; i++ {
 		s_start := time.Now()
 		splay.Search(input[i])
 		splay_time += time.Since(s_start)
@@ -64,35 +65,25 @@ func benchmark(size int, method string) (time.Duration, time.Duration) {
 		vEM_time += time.Since(v_start)
 	}
 
-	//fmt.Printf("Input Size: %d, Sampling Distribution: Uniform\n", current_range)
-	//fmt.Printf("Splay: ")
-	//fmt.Println(splay_time)
-	//fmt.Printf("vEM: ")
-	//fmt.Println(vEM_time)
 	return splay_time, vEM_time
 }
 
-func benchmark_set(input_type string, input_size int, number_of_input_files int) [][]time.Duration {
-	var input_file string
-	switch input_size {
-	case SMALL:
-		input_file = "small.in."
-	case MEDIUM:
-		input_file = "medium.in."
-	case LARGE:
-		input_file = "large.in."
-	}
-	
-	var path string = "inputs/" + input_type + "/" + input_file
+func benchmark_set(method string) [][]time.Duration {
 
-	// initializing the 2d slice
-	var result [][]time.Duration = make([][]time.Duration, number_of_input_files)
-	for i := range number_of_input_files {
-		result[i] = make([]time.Duration, 2)
+	var result [][]time.Duration = make([][]time.Duration, NUMBER_OF_OUTPUTS)
+	for i := range result {
+		result[i] = make([]time.Duration, 3)
 	}
 
-	for i := range number_of_input_files {
-		result[i][0], result[i][1] = benchmark(input_size, path+strconv.Itoa(i+1))
+	var index int = 0
+
+	for i := 2; i <= MAX_TREE_SIZE; i *= 2 {
+		for path := 1; path < 51; path++ {
+			result[index][0] = time.Duration(i)
+			result[index][1], result[index][2] = benchmark(i, method+"/"+strconv.FormatInt(int64(path), 10))
+			index++
+		}
+		fmt.Println("break")
 	}
 
 	return result
@@ -108,12 +99,11 @@ func contains(args []string, target string) bool {
 }
 
 func write_to_output(path string, array [][]time.Duration) {
-	_ = os.Mkdir("output", 0750)
-	var output, _ = os.OpenFile("output/"+path, os.O_CREATE|os.O_APPEND|os.O_WRONLY, 0644)
+	var output, _ = os.OpenFile("../src/output/"+path, os.O_CREATE|os.O_APPEND|os.O_WRONLY, 0644)
 
-	for i := 0; i < NUMBER_OF_INPUT_FILES; i++ {
+	for i := 0; i < NUMBER_OF_OUTPUTS; i++ {
 		_, err := output.WriteString(strconv.FormatInt(int64(array[i][0]), 10) + " " +
-			strconv.FormatInt(int64(array[i][1]), 10) + "\n")
+			strconv.FormatInt(int64(array[i][1]), 10) + " " + strconv.FormatInt(int64(array[i][2]), 10) + "\n")
 
 		if err != nil {
 			log.Fatal(err)
@@ -132,17 +122,17 @@ func main() {
 	fmt.Printf("Performing Searches...")
 
 	if contains(cmd_args, BENCHMARK_ALL) {
-		write_to_output("uniform_small.out", benchmark_set("uniform", SMALL, NUMBER_OF_INPUT_FILES))
-		write_to_output("uniform_medium.out", benchmark_set("uniform", MEDIUM, NUMBER_OF_INPUT_FILES))
+		write_to_output("uniform", benchmark_set("uniform"))
+		write_to_output("uniform", benchmark_set("uniform"))
 
-		write_to_output("mtf_opt_small.out", benchmark_set("mtf_opt", SMALL, NUMBER_OF_INPUT_FILES))
-		write_to_output("mtf_opt_medium.out", benchmark_set("mtf_opt", SMALL, NUMBER_OF_INPUT_FILES))
+		write_to_output("mtf_opt", benchmark_set("mtf_opt"))
+		write_to_output("mtf_opt", benchmark_set("mtf_opt"))
 
 	} else if contains(cmd_args, BECNHMARK_UNIFORM) {
-		write_to_output("uniform_small.out", benchmark_set("mtf_opt", SMALL, NUMBER_OF_INPUT_FILES))
-		write_to_output("uniform_medium.out", benchmark_set("mtf_opt", SMALL, NUMBER_OF_INPUT_FILES))
+		write_to_output("uniform_small.out", benchmark_set("mtf_opt"))
+		write_to_output("uniform_medium.out", benchmark_set("mtf_opt"))
 	} else {
-		write_to_output("mtf_opt_small.out", benchmark_set("uniform", SMALL, NUMBER_OF_INPUT_FILES))
-		write_to_output("mtf_opt_medium.out", benchmark_set("uniform", MEDIUM, NUMBER_OF_INPUT_FILES))
+		write_to_output("mtf_opt_small.out", benchmark_set("uniform"))
+		write_to_output("mtf_opt_medium.out", benchmark_set("uniform"))
 	}
 }
